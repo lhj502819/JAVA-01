47a5321e9e795f3b5c5f26bb7b133c3d04776495
李宏健 <lhj502819@163.com>
2021-03-04 20:33:13 +0800

    Add Test Insert 100W case

--- Week_07/lesson01/src/test/java/mysql/TestInsert.java
+++ Week_07/lesson01/src/test/java/mysql/TestInsert.java
@@ -30,21 +30,24 @@ public class TestInsert {
     @Autowired
     private HikariDataSource hikariDataSource;
 
+    Timestamp timestamp ;
 
     @Before
     public void before() {
-
+        ZoneId zone = ZoneId.systemDefault();
+        Instant instant = LocalDateTime.now().atZone(zone).toInstant();
+        timestamp = new Timestamp(instant.toEpochMilli());
     }
 
     @Test
     public void insert() {
-        ExecutorService executorService = Executors.newFixedThreadPool(500);
+        ExecutorService executorService = Executors.newFixedThreadPool(1000);
         CountDownLatch countDownLatch = new CountDownLatch(1000000);
-        System.out.println(LocalTime.now());
-        for (int j = 0 ; j < 500 ; j++) {
+        LocalTime startTime = LocalTime.now();
+        for (int j = 0 ; j < 1000 ; j++) {
             int finalJ = j;
             executorService.submit(() -> {
-                for (int i = 0 ; i < 2000 ; i++) {
+                for (int i = 0 ; i < 1000 ; i++) {
                     int finalI = i;
                     Connection connection = null;
                     try {
@@ -56,21 +59,18 @@ public class TestInsert {
                                 "buy_price,payment_time," +
                                 "delivery_time,receiver_time," +
                                 "user_id) values(?,?,?,?,?,?,?,?,?,?,?)";
-                        Random random = new Random(999999);
-                        ZoneId zone = ZoneId.systemDefault();
-                        Instant instant = LocalDateTime.now().atZone(zone).toInstant();
                         statement = connection.prepareStatement(sql);
-                        statement.setString(1,"ON" + random.nextInt() + finalJ + finalI);
+                        statement.setString(1,"ON"  + finalJ + finalI);
                         statement.setLong(2 , 1L);
                         statement.setString(3,"iphone12 Pro Max");
                         statement.setString(4,"https://pgdt.gtimg.cn/gdt/0/EAAoQE3AGQAEsAAAJ0sBgPK30BQ2L_KWG.jpg/0?ck=b378ee8716b1e76ea1631e6359bd4d90&md5=b378ee8716b1e76ea1631e6359bd4d90");
-                        statement.setInt(5 , random.nextInt());
-                        statement.setInt(6 , random.nextInt());
-                        statement.setInt(7 , random.nextInt());
-                        statement.setTimestamp(8 , new Timestamp(instant.toEpochMilli()));
-                        statement.setTimestamp(9 , new Timestamp(instant.toEpochMilli()));
-                        statement.setTimestamp(10 , new Timestamp(instant.toEpochMilli()));
-                        statement.setLong(11 , random.nextLong());
+                        statement.setInt(5 , 555);
+                        statement.setInt(6 , 555);
+                        statement.setInt(7 , 555);
+                        statement.setTimestamp(8 , timestamp);
+                        statement.setTimestamp(9 , timestamp);
+                        statement.setTimestamp(10 , timestamp);
+                        statement.setLong(11 , 9999L);
                         statement.executeUpdate();
                     } catch (SQLException throwables) {
                         throwables.printStackTrace();
@@ -92,7 +92,7 @@ public class TestInsert {
             e.printStackTrace();
         }
 
-        System.out.println(LocalTime.now());
+        System.out.println("开始时间：" + startTime + " 结束时间" + LocalTime.now());
 
 
     }
