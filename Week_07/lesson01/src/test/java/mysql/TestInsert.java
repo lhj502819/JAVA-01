7ec151c2ba2e544bfcfbce289c4ce5e1abc64cc0
李宏健 <lhj502819@163.com>
2021-03-04 22:53:53 +0800

    Add Test Insert 100W case

--- Week_07/lesson01/src/test/java/mysql/TestInsert.java
+++ Week_07/lesson01/src/test/java/mysql/TestInsert.java
@@ -17,6 +17,7 @@ import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author li.hongjian
@@ -30,7 +31,7 @@ public class TestInsert {
     @Autowired
     private HikariDataSource hikariDataSource;
 
-    Timestamp timestamp ;
+    Timestamp timestamp;
 
     @Before
     public void before() {
@@ -40,48 +41,55 @@ public class TestInsert {
     }
 
     @Test
-    public void insert() {
-        ExecutorService executorService = Executors.newFixedThreadPool(1000);
-        CountDownLatch countDownLatch = new CountDownLatch(1000000);
-        LocalTime startTime = LocalTime.now();
-        for (int j = 0 ; j < 1000 ; j++) {
+    public void insert() throws SQLException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        long startTime = System.currentTimeMillis();
+        for (int j = 0; j < 5; j++) {
             int finalJ = j;
+            Connection connection = null;
+            connection = hikariDataSource.getConnection();
+            Connection finalConnection = connection;
+            String sql = "insert into t_order (order_no,product_id," +
+                    "product_name,product_image," +
+                    "quantity,origin_price," +
+                    "buy_price,payment_time," +
+                    "delivery_time,receiver_time," +
+                    "user_id) values(?,?,?,?,?,?,?,?,?,?,?)";
+            AtomicReference<PreparedStatement> statement = new AtomicReference<>(connection.prepareStatement(sql));
             executorService.submit(() -> {
-                for (int i = 0 ; i < 1000 ; i++) {
+                PreparedStatement preparedStatement = statement.get();
+                for (int i = 0; i < 200000; i++) {
                     int finalI = i;
-                    Connection connection = null;
                     try {
-                        connection = hikariDataSource.getConnection();
-                        PreparedStatement statement = null;
-                        String sql = "insert into t_order (order_no,product_id," +
-                                "product_name,product_image," +
-                                "quantity,origin_price," +
-                                "buy_price,payment_time," +
-                                "delivery_time,receiver_time," +
-                                "user_id) values(?,?,?,?,?,?,?,?,?,?,?)";
-                        statement = connection.prepareStatement(sql);
-                        statement.setString(1,"ON"  + finalJ + finalI);
-                        statement.setLong(2 , 1L);
-                        statement.setString(3,"iphone12 Pro Max");
-                        statement.setString(4,"https://pgdt.gtimg.cn/gdt/0/EAAoQE3AGQAEsAAAJ0sBgPK30BQ2L_KWG.jpg/0?ck=b378ee8716b1e76ea1631e6359bd4d90&md5=b378ee8716b1e76ea1631e6359bd4d90");
-                        statement.setInt(5 , 555);
-                        statement.setInt(6 , 555);
-                        statement.setInt(7 , 555);
-                        statement.setTimestamp(8 , timestamp);
-                        statement.setTimestamp(9 , timestamp);
-                        statement.setTimestamp(10 , timestamp);
-                        statement.setLong(11 , 9999L);
-                        statement.executeUpdate();
+
+                        preparedStatement.setString(1, "ON" + finalJ + finalI);
+                        preparedStatement.setLong(2, 1L);
+                        preparedStatement.setString(3, "iphone12 Pro Max");
+                        preparedStatement.setString(4, "https://pgdt.gtimg.cn/gdt/0/EAAoQE3AGQAEsAAAJ0sBgPK30BQ2L_KWG.jpg/0?ck=b378ee8716b1e76ea1631e6359bd4d90&md5=b378ee8716b1e76ea1631e6359bd4d90");
+                        preparedStatement.setInt(5, 555);
+                        preparedStatement.setInt(6, 555);
+                        preparedStatement.setInt(7, 555);
+                        preparedStatement.setTimestamp(8, timestamp);
+                        preparedStatement.setTimestamp(9, timestamp);
+                        preparedStatement.setTimestamp(10, timestamp);
+                        preparedStatement.setLong(11, 9999L);
+                        preparedStatement.addBatch();
                     } catch (SQLException throwables) {
                         throwables.printStackTrace();
-                    }finally {
-                        try {
-                            connection.close();
-                        } catch (SQLException throwables) {
-                            throwables.printStackTrace();
-                        }
                     }
-                    countDownLatch.countDown();
+                }
+                try {
+                    preparedStatement.executeBatch();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }finally {
+                    try {
+                        finalConnection.close();
+                        countDownLatch.countDown();
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
                 }
             });
         }
@@ -92,52 +100,8 @@ public class TestInsert {
             e.printStackTrace();
         }
 
-        System.out.println("开始时间：" + startTime + " 结束时间" + LocalTime.now());
-
-
-    }
-
-
-    @Test
-    public void test2() {
-        for (int i = 0 ; i < 1000000 ; i++) {
-            Connection connection = null;
-            try {
-                connection = hikariDataSource.getConnection();
-                PreparedStatement statement = null;
-                String sql = "insert into t_order (order_no,product_id," +
-                        "product_name,product_image," +
-                        "quantity,origin_price," +
-                        "buy_price,payment_time," +
-                        "delivery_time,receiver_time," +
-                        "user_id) values(?,?,?,?,?,?,?,?,?,?,?)";
-                Random random = new Random(999999);
-                ZoneId zone = ZoneId.systemDefault();
-                Instant instant = LocalDateTime.now().atZone(zone).toInstant();
-                statement = connection.prepareStatement(sql);
-                statement.setString(1,"ON" + random.nextInt() + i);
-                statement.setLong(2 , 1L);
-                statement.setString(3,"iphone12 Pro Max");
-                statement.setString(4,"https://pgdt.gtimg.cn/gdt/0/EAAoQE3AGQAEsAAAJ0sBgPK30BQ2L_KWG.jpg/0?ck=b378ee8716b1e76ea1631e6359bd4d90&md5=b378ee8716b1e76ea1631e6359bd4d90");
-                statement.setInt(5 , random.nextInt());
-                statement.setInt(6 , random.nextInt());
-                statement.setInt(7 , random.nextInt());
-                statement.setTimestamp(8 , new Timestamp(instant.toEpochMilli()));
-                statement.setTimestamp(9 , new Timestamp(instant.toEpochMilli()));
-                statement.setTimestamp(10 , new Timestamp(instant.toEpochMilli()));
-                statement.setLong(11 , random.nextLong());
-                statement.executeUpdate();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }finally {
-                try {
-                    connection.close();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-        }
+        System.out.println("耗时：" + (System.currentTimeMillis() - startTime));
+        executorService.shutdownNow();
 
     }
-
 }
